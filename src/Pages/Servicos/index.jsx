1cf950179b160501d0c0769e5340f5edb8cf407d
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-02-01 11:51:54 -0300

    completed the services page

--- src/Pages/Servicos/index.jsx
+++ src/Pages/Servicos/index.jsx
@@ -3,6 +3,11 @@ import Container from '../../components/Container'
 import Footer from '../../components/Footer'
 import Styles from './Servicos.module.css'
 import Titulo from '../../components/Titulo'
+import Card from '../../components/Card'
+import Servico1 from './img/servico1.png'
+import Servico2 from './img/servico2.png'
+import Servico3 from './img/servico3.png'
+import Servico4 from './img/servico4.png'
 
 function Servicos() {
   return (
@@ -14,48 +19,49 @@ function Servicos() {
         <Container>
             <div className={Styles.content}>
 
-              <div className={Styles.textos}>
-                <Titulo title='nossos serviços' />
-                <p>
-                  Precisando de limpeza e higienização especializada para estofados, cortinas, poltronas, sofás,
-                  cadeiras, colchões, carpetes, tapetes, divisórias e veículos com excelência na qualidade do
-                  serviço, segurança e responsabilidade de quem esta no ramo a mais de 10 anos? Você esta no
-                  lugar certo... conheça aqui um pouco mais da Renovadora Fast Clean. Executamos tudo no local,
-                  sendo assim você pode acompanhar todo o procedimento de limpeza. Nossos equipamentos são de
-                  uso exclusivo e passam por rigorosos testes antes de qualquer demanda.
-                </p>
-              </div>
-
-              <div className={Styles.cards}>
-                <div className={Styles.card}>
-                  {/* <img src={Servico1} alt="Serviço1" className={Styles.imagem} /> */}
-                  <h2>Sofás</h2>
-                  <p>Tratamento de primeira qualidade para sofás e estofados, clean e sem ácaros.
-                    Todos os serviços são executados no local.
-                  </p>
+                {/* area dos textos */}
+                <div className={Styles.areatextos}>
+                    <Titulo title='nossos serviços' />
+                    <p>
+                      Precisando de limpeza e higienização especializada para estofados, cortinas, poltronas, sofás,
+                      cadeiras, colchões, carpetes, tapetes, divisórias e veículos com excelência na qualidade do
+                      serviço, segurança e responsabilidade de quem esta no ramo a mais de 10 anos? Você esta no
+                      lugar certo... conheça aqui um pouco mais da Renovadora Fast Clean. Executamos tudo no local,
+                      sendo assim você pode acompanhar todo o procedimento de limpeza. Nossos equipamentos são de
+                      uso exclusivo e passam por rigorosos testes antes de qualquer demanda.
+                    </p>
                 </div>
 
-                <div className={Styles.card}>
-                  {/* <img src={Servico1} alt="Serviço1" className={Styles.imagem} /> */}
-                  <h2>Colchões</h2>
-                  <p>Qualidade de vida com um sono renovador livre de impurezas, ácaros e fungos. Todos os nossos serviços são executados no local.
-                  </p>
+                {/* area dos cards */}
+                <div className={Styles.cards}>
+                  
+                    <Card 
+                      imagem    ={Servico1} 
+                      titulo    ="Sofás"
+                      descricao ="Tratamento de primeira qualidade para sofás e estofados ficarem 
+                                  clean e sem ácaros. Todos os serviços são executados no local."
+                    />
+                    <Card 
+                      imagem    ={Servico2} 
+                      titulo    ="Colchões"
+                      descricao ="Qualidade de vida com um sono renovador livre de impurezas, ácaros
+                                  e fungos. Todos os nossos serviços são executados no local."
+                    />
+                    <Card 
+                      imagem    ={Servico3} 
+                      titulo    ="Carpetes"
+                      descricao ="Método inovador para remoção de ácaros limpeza de tapetes com total segurança. 
+                                  Todos os serviços são executados no local."
+                    />
+                    <Card 
+                      imagem    ={Servico4} 
+                      titulo    ="Cortinas"
+                      descricao ="Suas cortinas impecáveis com a cor natural sem manchas ou avarias.Todos os 
+                                  nossos serviços são executados no local."
+                    />
+                    
                 </div>
-
-                <div className={Styles.card}>
-                  {/* <img src={Servico1} alt="Serviço1" className={Styles.imagem} /> */}
-                  <h2>Carpetes</h2>
-                  <p> Método inovador para remoção de ácaros limpeza de tapetes com total segurança. Todos os serviços são executados no local.
-                  </p>
-                </div>
-
-                <div className={Styles.card}>
-                  {/* <img src={Servico1} alt="Serviço1" className={Styles.imagem} /> */}
-                  <h2>Cortinas</h2>
-                  <p>Suas cortinas impecáveis com a cor natural sem manchas ou avarias.Todos os nossos serviços são executados no local.
-                  </p>
-                </div>
-              </div>
+                
             </div>
         </Container>
 
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,10 +3,16 @@ import Styles from './Card.module.css'
 function Card(props)
 {
     return(
-        <section className={Styles.seccard}> 
+        <section className={Styles.areacards}> 
         
-          
-        
+            <div className={Styles.card}>
+                <img src={props.imagem} alt="Serviços" className={Styles.imagem} />
+                <div className={Styles.areatextos}>
+                    <h2>{props.titulo}</h2>
+                    <p>{props.descricao}</p>
+                </div>                
+            </div>
+                   
         </section>
     )
 }
