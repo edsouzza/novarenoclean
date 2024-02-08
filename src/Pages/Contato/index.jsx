d53c3266979d89a95566f221beb7d26eaf3a8784
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-02-08 12:55:17 -0300

    update page contato

--- src/Pages/Contato/index.jsx
+++ src/Pages/Contato/index.jsx
@@ -1,15 +1,25 @@
+import Styles from './Contato.module.css'
 import Header from '../../components/Header'
 import Container from '../../components/Container'
 import Footer from '../../components/Footer'
+import Titulo from '../../components/Titulo'
 
 function Contato()
-{
+{ 
     return(
-        <>
+      <>
         <Header />    
-          <Container>
-            <h1 style={{color:"var(--white)"}}>Contato</h1>
-          </Container>
+          <section className={Styles.contato}>
+            <Container>
+                <div className={Styles.content}>
+                  <div className={Styles.areaConteudo}>
+                    
+                      <Titulo title='Contato' className={Styles.title}/>
+                                            
+                  </div>
+                </div>
+            </Container>
+          </section>
         <Footer />      
       </>
     )
