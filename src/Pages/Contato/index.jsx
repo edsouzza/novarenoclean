4e402808901ebcdef96629d7861c4371c90579cd
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-02-09 13:53:19 -0300

    maked page contatos

--- src/Pages/Contato/index.jsx
+++ src/Pages/Contato/index.jsx
@@ -17,26 +17,39 @@ function Contato()
                       <Titulo title='Contato' className={Styles.title}/>
                       <div className={Styles.areaBox}>
                           <div className={Styles.boxLeft}>
-                              <h2>redes sociais</h2>
-                              <p><a href="http://www.facebook.com/edsouzza" target="_blank" rel="noreferrer noopener"><BsFacebook className={Styles.icone}/></a> Acompanhe-nos pela nossa rede social para mais informações.</p>                              
-                              <h2>telefones</h2>
-                              <p><BsTelephone  className={Styles.icone}/> (011) 3921-3996 | (011) 95425-4706</p>
-                              <p><BsEnvelope   className={Styles.icone}/> renovadorarenoclean@gmail.com</p>
-                              <h2>Desenvolvido pela Edsof Informática</h2>
-                              <h3>® Todos os direitos reservados</h3>
-                              <h4>edsouzza@hotmail.com | (011) 99475-6517</h4>
+                              <Titulo title="redes sociais"/>                              
+                              <p className={Styles.textoRedes}><a href="http://www.facebook.com/edsouzza" target="_blank" rel="noreferrer noopener"><BsFacebook className={Styles.icone}/></a> Acompanhe-nos pela nossa rede social para mais informações.</p>                              
+                              
+                              <Titulo title="telefones"/>
+                              <p className={Styles.textoRedes}><BsTelephone className={Styles.icone}  />(011) 3921-3996 | (011) 95425-4706</p>
+                              
+                              <Titulo title="e-mail"/>
+                              <p className={Styles.textoRedes}><BsEnvelope className={Styles.icone}/>renovadorarenoclean@gmail.com</p>
+                              
+                              <h3 className={Styles.textoDesenv}>Desenvolvido pela Edsof Informática</h3>
+                              
+                              <h4 className={Styles.textoDesenv}>edsouzza@hotmail.com | (011) 99475-6517</h4>
+                              
+                              <h4 className={Styles.textoDesenv}>® Todos os direitos reservados</h4>
                           </div>
+                          
                           <div className={Styles.boxRight}>
-                              <h2>fale conosco</h2>
-                              <p>Dúvidas, criticas ou sugestões? Entre em contato conosco seu feedback é muito importante para nós!</p>
-                              <form action="">
-                                <input type="text" name='nome' placeholder='Seu nome'/>
-                                <input type="email" name='email' placeholder='Seu e-mail'/>
-                                <input type="text" name='assunto' placeholder='Assunto'/>
-                                <textarea name="mensagem" cols="30" rows="10" placeholder='Mensagem max.50 caracteres'></textarea>
-                                <button type='submit'>Enviar</button>
+                              <Titulo title="fale conosco" className={Styles.titleFaleConosco}/>
+                              <p className={Styles.textoFaleConosco}>Dúvidas, criticas ou sugestões? Fale conosco!</p>
+                              
+                              <form className={Styles.formulario}>
+                                                                  
+                                  <input type="text" name='nome' placeholder='Seu nome' required />                                 
+                                  <input type="email" name='email' placeholder='Seu e-mail' required />                                  
+                                  <input type="text" name='assunto' placeholder='Assunto' required />
+                                  <textarea name="mensagem" cols="30" rows="6" placeholder='Mensagem max.50 caracteres' style={{resize: "none"}}  ></textarea>
+                                  <div className={Styles.areaButton}>
+                                      <button type='submit' className={Styles.btnEnviar}>Enviar</button>
+                                  </div>
+                                
                               </form>
                           </div>
+
                       </div>                                            
                   </div>
                 </div>
